ffa451cccb9ccad62a069276dc1db77884808549
Sherklan12e <97853791+Sherklan12e@users.noreply.github.com>
2025-04-23 17:48:32 -0300

    calculadora terminada

--- calculadora.go
+++ calculadora.go
@@ -5,29 +5,40 @@ import (
 )
 
 func main() {
-	var num1, num2 float64
-	var operacion string
-	fmt.Print("Digite o primeiro número: ")
-	fmt.Scan(&num1)
-	fmt.Print("Digite el segundo número: ")
-	fmt.Scan(&num2)
-	fmt.Print("Digite a operacion (+, -, *, /): ")
-	fmt.Scan(&operacion)
-	switch operacion {
-	case "+":
-		fmt.Println("Resultado: ", num1+num2)
+	a := 0
+	for a < 1 {
 
-	case "-":
-		fmt.Println("Resultado: ", num1-num2)
-	case "*":
-		fmt.Println("Resultado: ", num1*num2)
-	case "/":
-		if num2 != 0 {
-			fmt.Println("Resultado: ", num1/num2)
-		} else {
-			fmt.Println("Error: Division by zero is not allowed.")
+		var num1, num2 float64
+		var operacion string
+		fmt.Print("Digite o primeiro número: ")
+		fmt.Scan(&num1)
+		fmt.Print("Digite el segundo número: ")
+		fmt.Scan(&num2)
+		v, err := fmt.Scanf("%f", &num1, &num2)
+		if num1 == 0 && num2 == 0 || v != 1 && err == nil || v != 1 && err == nil {
+			fmt.Println("Error: No se puede realizar operaciones con cero.")
+			break
 		}
-	default:
-		fmt.Println("Error: Operacion no valida.")
+		fmt.Print("Digite a operacion (+, -, *, /): ")
+		fmt.Scan(&operacion)
+		switch operacion {
+		case "+":
+			fmt.Println("Resultado: ", num1+num2)
+
+		case "-":
+			fmt.Println("Resultado: ", num1-num2)
+		case "*":
+			fmt.Println("Resultado: ", num1*num2)
+		case "/":
+			if num2 != 0 {
+				fmt.Println("Resultado: ", num1/num2)
+			} else {
+				fmt.Println("Error: Division by zero is not allowed.")
+			}
+		default:
+			fmt.Println("Error: Operacion no valida.")
+			a = 1
+		}
 	}
+
 }
